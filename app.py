f410b77c78ed95b77a1141b5a46944690b876908
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-09-29 14:50:04 +0200

    Create basic page structure and set up

--- app.py
+++ app.py
@@ -3,7 +3,18 @@ import pandas as pd
 import streamlit as st
 
 def main():
-    #things happen
+    st.title("🎵 Music recommender 🎵")
+
+    # Session set up
+    if "genre" not in st.session_state:
+        # Session variables
+        st.session_state.genre = ""
+        st.session_state.trendy = f.find_top100()
+
+    st.session_state.genre = st.selectbox("Choose a genre", ["Trendy", "Other"])
+
+    if st.session_state.genre == "Trendy":
+        f.play_song()#song_id
 
 
 if __name__ == '__main__':
